a983cbf9af6a7f735994897bc31d9083b041bfa5
Andrew Maurer <git@maurer.me>
2012-12-13 15:25:05 -0500

    added filterbetween

--- main.js
+++ main.js
@@ -2,10 +2,6 @@ var fs = require("fs");
 var LogMassage = require("./log-massage.js");
 
 var logFilePath = "X:\\JRun4\\servers\\OEMLR_Sean\\cfusion.ear\\cfusion.war\\WEB-INF\\cfusion\\logs\\Dean.log";
-var filterRange = [
-	new Date("12/03/12").getTime(),
-	new Date("12/04/12").getTime()
-];
 
 
 fs.readFile(logFilePath, function(e, data){
